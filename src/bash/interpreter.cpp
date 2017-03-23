07d95b1ca75d42fc0b15fea21ab87260cde43c21
Hazurl <ricoujules69@gmail.com>
2017-03-23 18:38:01 +0100

    [Bash] Preparation

--- src/bash/interpreter.cpp
+++ src/bash/interpreter.cpp
@@ -1,8 +1,5 @@
 #include "interpreter.h"
 
-#include <iostream>
-#include <string>
-
 using namespace bash;
 
 //      =====   INTERPRETER    =====
@@ -16,11 +13,51 @@ Interpreter::~Interpreter () {
 }
 
 void Interpreter::interactive () {
-    std::cout << INTERACTIVE_PREFIXE;
+    std::string cmd;
+
+    while (!std::cin.eof()) {
+        std::cout << INTERACTIVE_PREFIXE;
+		std::getline(std::cin, cmd);
+
+        if (cmd == INTERACTIVE_EXIT_CMD)
+            break;
+
+        run(cmd);
+	}
 
     std::cout << std::endl;
 }
 
+std::string Interpreter::run(std::string code) {
+    if (this->ast)
+        delete this->ast;
+
+    this->tokens.clear();
+
+    tokenize(code);
+    parse();
+    return evaluate();
+}
+
 void Interpreter::runTest () {
     std::cout << "TEST" << std::endl;
+}
+
+void Interpreter::tokenize (std::string code) {
+
+    std::string curTokenContent = "";
+
+    for (char& c : code) {
+        
+          
+    }
+    std::cout << std::endl;
+}
+
+void Interpreter::parse () {
+
+}
+
+std::string Interpreter::evaluate () {
+    return "";
 }
--- src/bash/interpreter.h
+++ src/bash/interpreter.h
@@ -1,18 +1,62 @@
 #ifndef __BASH_INTERPRETER__
 #define __BASH_INTERPRETER__
 
+#include <string>
+#include <iostream>
+#include <vector>
+
 namespace bash {
 
 #define INTERACTIVE_PREFIXE "~> "
+#define INTERACTIVE_EXIT_CMD "exit"
 
 class Interpreter {
 public: 
-                Interpreter                 ();
-                ~Interpreter                ();
+                            Interpreter                 ();
+                            ~Interpreter                ();
 
-    void        interactive                 ();
-    void        runTest                     ();
-};
+    void                    interactive                 ();
+    void                    runTest                     ();
+    std::string             run                         (std::string code);
+private:  
+
+    enum class              charType;
+    struct                  Token;
+    class                   AST;
+
+    std::vector<Token>      tokens;
+    AST*                    ast                         = nullptr;
+
+    void                    tokenize                    (std::string code);
+    void                    parse                       ();
+    std::string             evaluate                    ();
+
+    enum class charType {
+        ALPHA, NUM, DOT, 
+        PLUS, MINUS, DIV, MUL, MOD,
+        PARENTHESIS_LEFT, PARENTHESIS_RIGHT
+    };
+
+
+struct Token {
+public:
+    enum class              Type;
+
+    Token::Type             type;
+    std::string             content;
+    unsigned long           position;
+
+    enum class Type {
+        PLUS, MINUS, DIV, MUL, MOD,
+        PARENTHESIS_LEFT, PARENTHESIS_RIGHT
+    };
+}; // struct Token
+
+class AST {
+
+}; // class AST
+
+}; // class Interpreter
 
 }; // namespace bash
 
